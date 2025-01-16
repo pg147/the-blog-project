6612d1de81f5cceef42a357cea805349ae950e85
pg147 <xyzprathamesh147@gmail.com>
2025-01-16 20:20:43 +0530

    Initial commit - Base Project Setup done

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="font-medium">Hi from The Blog Project !</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "regular" : ["Gilroy-Regular"],
+        "medium" : ["Gilroy-Medium"],
+        "semibold" : ["Gilroy-Semibold"],
+        "bold" : ["Gilroy-Bold"]
+      },
+      colors: {
+        primary: "#FE416F"
+      },
+      boxShadow: {
+        intense: "0px 0px 16px rgba(17, 17, 26, 0.1)"
+      }
+    },
+  },
+  plugins: [],
+}
+
