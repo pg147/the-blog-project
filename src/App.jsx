b6906f03a8f9ed7a81ddc07a0d2e502a95e27cd2
pg147 <xyzprathamesh147@gmail.com>
2025-01-17 16:29:43 +0530

    User Auth Check method added with Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,38 @@
+// React Imports
+import { useEffect, useState } from "react"
+
+// React Redux imports
+import { useDispatch } from "react-redux";
+
+// Auth.js from Appwrite
+import authService from "./appwrite/auth";
+
+// Auth Slice Methods
+import { login, logout } from "./store/authSlice";
+
+// Icon library - Lucide React
+import { Loader2 } from "lucide-react";
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Verifying if user is logged in
+    authService.getUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else dispatch(logout());
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <div className="h-screen w-full bg-white flex items-center justify-center">
+    <Loader2 className="text-primary size-6 animate-spin" />
+  </div>
+
   return (
     <div>
       <h1 className="font-medium">Hi from The Blog Project !</h1>
