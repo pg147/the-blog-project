b1a950baf0001d6f960b839860b29afe4c741bd2
pg147 <xyzprathamesh147@gmail.com>
2025-01-17 23:31:38 +0530

    Login Page added

---
+++ src/components/Login.jsx
@@ -0,0 +1,95 @@
+// React imports
+import { useState } from "react";
+
+// React Hook Form
+import { useForm } from "react-hook-form";
+
+// React Router
+import { Link, useNavigate } from "react-router-dom";
+
+// React Redux 
+import { useDispatch } from "react-redux";
+
+// Auth Service from appwrite
+import authService from "../appwrite/auth.js";
+
+// Auth Slice method
+import { login } from "../store/authSlice.js";
+
+// Components
+import { Input, Button, Logo } from "./index.js";
+
+// Custom Icons
+import { LockIcon, MailIcon } from "../constants/icons.js";
+
+export default function Login() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
+    const [error, setError] = useState("");
+
+    const handleLogin = async (data) => {
+        try {
+            const session = await authService.loginUser(data);
+
+            if (session) {
+                const userData = authService.getUser();
+                if (userData) dispatch(login(userData));
+                navigate("/");
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
+    return (
+        <div className="w-full md:max-w-xl shadow-intense rounded-xl">
+            {/* Logo */}
+            <div className="w-fit mx-auto"><Logo /></div>
+
+            {/* Heading */}
+            <h1 className="font-semibold text-center">Sign in to your <span className="text-primary">account</span></h1>
+
+            {/* Error message */}
+            {error.length > 0 && <p className="text-sm font-medium text-red-700">Oops ! {error}</p>}
+
+            {/* Form */}
+            <form onSubmit={handleSubmit} className="mt-6">
+                {/* Email */}
+                <Input
+                    icon={<MailIcon />}
+                    type="email"
+                    placeholder="Enter your email"
+                    {...register("email", {
+                        required: true,
+                        pattern: /^([\w.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/
+                    })}
+                />
+
+                {/* Password */}
+                <Input
+                    icon={<LockIcon />}
+                    type="password"
+                    className="mt-3"
+                    placeholder="Enter your password"
+                    {...register("password", {
+                        required: true,
+                        min: 6,
+                        max: 10
+                    })}
+                />
+
+                {/* Button */}
+                <Button type="submit" title="Sign in" variant="primary" className="w-full" />
+            </form>
+
+            {/* Quicklink */}
+            <p className="text-center text-sm font-medium">
+                Don&apos;t have an account ?
+                <Link to={"/sign-up"}>
+                    <span className="text-primary lg:hover:underline">Sign up</span>
+                </Link>
+            </p>
+        </div>
+    )
+}
