215710bcab738cbc75bda08947ecf32a2ada345c
pg147 <xyzprathamesh147@gmail.com>
2025-01-17 23:31:23 +0530

    export index updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import Postcard from "./Postcard";
 
-export { Logo, Header, Footer };
+export { Logo, Header, Footer, Button, Input, Select, Postcard };
