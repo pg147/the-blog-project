c2626b2d7d3e5abc1d4796ef6352324a2e46eaf2
pg147 <xyzprathamesh147@gmail.com>
2025-01-17 17:58:03 +0530

    Logo Component created

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <div className="h-fit w-fit flex items-center font-semibold text-xl">
+            <h1>Blogi {""}</h1>
+            <span className="text-primary">.fy</span>
+        </div>
+    )
+}
