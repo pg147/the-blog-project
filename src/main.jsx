4c4a762ff5fe15df25bbb22eac39fa8289b6b491
pg147 <xyzprathamesh147@gmail.com>
2025-01-18 20:50:39 +0530

    Router created

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,63 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AddPost, AllPosts, EditPost, Home, Login, Post, Signup } from './pages';
+import { AuthLayout } from './components';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <AuthLayout authentication={false} >
+          <Login />
+        </AuthLayout>
+      },
+      {
+        path: '/signup',
+        element: <AuthLayout authentication={false}>
+          <Signup />
+        </AuthLayout>
+      },
+      {
+        path: '/add-post',
+        element: <AuthLayout authentication>
+          <AddPost />
+        </AuthLayout>
+      },
+      {
+        path: '/all-posts',
+        element: <AuthLayout authentication>
+          <AllPosts />
+        </AuthLayout>
+      },
+      {
+        path: '/post/edit/:slug',
+        element: <AuthLayout authentication>
+          <EditPost />
+        </AuthLayout>
+      },
+      {
+        path: '/post/:slug',
+        element: <AuthLayout authentication>
+          <Post />
+        </AuthLayout>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </Provider>
 )
