99172eefd8dd5a31157d9165ff6239ce15581370
pg147 <xyzprathamesh147@gmail.com>
2025-01-18 19:49:50 +0530

    Auth Pages added

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import { Login as LoginComponent } from '../components/index';
+
+export default function Login() {
+  return (
+    <div>
+        <LoginComponent />
+    </div>
+  )
+}
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import { Signup as SignupComponent } from '../components/index';
+
+export default function Signup() {
+  return (
+    <div>
+        <SignupComponent />
+    </div>
+)
+}
