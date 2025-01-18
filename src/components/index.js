13f5888082685da11c09df2a80b01ade79ce3730
pg147 <xyzprathamesh147@gmail.com>
2025-01-18 20:50:18 +0530

    Index imports updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Header from "./Header/Header.jsx";
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import Postcard from "./Postcard";
 import PostForm from "./Post-Form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 import RTE from "./RTE";
 
-export { Logo, Header, Footer, Button, Input, Select, Postcard, RTE, PostForm };
+export { Logo, Login, Signup, Header, Button, Input, Select, Postcard, RTE, PostForm, AuthLayout };
